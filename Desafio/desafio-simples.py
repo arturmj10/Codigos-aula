20e0e48055f62fa697458c0d4524c41659fca5f2
arturmj <arturmj10@gmail.com>
2024-04-19 14:59:25 -0300

    criei outro ex porque achei outra maneira de fazer sem o append

---
+++ Desafio/desafio-simples.py
@@ -0,0 +1,13 @@
+#ler arquivos - pegar um arquivo txt e dar um print nele
+try:
+    with open('C:/Users/artur/OneDrive/Área de Trabalho/arquivos/1 fase/Algoritmos e Programacao/Codigos-aula/Desafio/ListaDesafio.txt', 'r', encoding='utf-8') as arquivo:
+        linhas = [linha.strip() for linha in arquivo.readlines()]
+            
+        linhas.sort()
+
+        for i, linha in enumerate(linhas):
+            print(f"{i+1}. {linha}")
+except FileNotFoundError:
+    print(f"O arquivo não foi encontrado.")
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
--- Desafio/desafio.py
+++ Desafio/desafio.py
@@ -1,12 +1,16 @@
 #ler arquivos - pegar um arquivo txt e dar um print nele
-arquivo = 'ListaAlgoritmos.txt'
+arquivo = 'C:/Users/artur/OneDrive/Área de Trabalho/arquivos/1 fase/Algoritmos e Programacao/Codigos-aula/Desafio/ListaDesafio.txt'
 lista = []
 
 with open(arquivo, 'r', encoding="utf-8") as file:
-    data = file.read()
     n_linhas = sum(1 for line in file)
-    print(sum(1 for line in file))
+    print(n_linhas)
 
-"""for indice, linha in enumerate(data):
-    print(indice +1, linha)"""
+with open(arquivo, 'r', encoding="utf-8") as file:    
+    for i in range(n_linhas):
+        lista.append(file.readline())
     
+lista.sort()
+
+for i, post in enumerate(lista):
+    print(f'{i+1}- {post}')
