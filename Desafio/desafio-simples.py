3f8216bc93263ad2977d5e52fcc5dfc3ed92bc5a
arturmj <arturmj10@gmail.com>
2024-04-19 15:03:02 -0300

    Update desafio-simples.py

--- Desafio/desafio-simples.py
+++ Desafio/desafio-simples.py
@@ -1,11 +1,11 @@
 #ler arquivos - pegar um arquivo txt e dar um print nele
 try:
     with open('C:/Users/artur/OneDrive/Área de Trabalho/arquivos/1 fase/Algoritmos e Programacao/Codigos-aula/Desafio/ListaDesafio.txt', 'r', encoding='utf-8') as arquivo:
-        linhas = [linha.strip() for linha in arquivo.readlines()]
+        lista = [linha.strip() for linha in arquivo.readlines()]
             
-        linhas.sort()
+        lista.sort()
 
-        for i, linha in enumerate(linhas):
+        for i, linha in enumerate(lista):
             print(f"{i+1}. {linha}")
 except FileNotFoundError:
     print(f"O arquivo não foi encontrado.")
